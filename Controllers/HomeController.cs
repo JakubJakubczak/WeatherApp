b70ad1f8947f044abef52de05e471c8fd708c02d
krzmak <krzysztof.mak.2001@gmail.com>
2024-04-22 23:34:07 +0200

    some changes to how we show weather

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,24 +16,7 @@ namespace WeatherApplication.Controllers
 
         public async Task<IActionResult> Index()
         {
-            double latitude = 51.11908339691092;
-            double longitude = 17.126694053556225;
-            var weather = await ApiService.GetWeather(latitude, longitude);
-
-            // Konwersja temperatury z Fahrenheitów na stopnie Celsiusza
-            foreach (var forecast in weather.list)
-            {
-                forecast.main.temp = Math.Round(ConvertKelvinToCelsius(forecast.main.temp),1);
-                forecast.main.temp_min = Math.Round(ConvertKelvinToCelsius(forecast.main.temp_min),1);
-                forecast.main.temp_max = Math.Round(ConvertKelvinToCelsius(forecast.main.temp_max),1);
-            }
-
-            return View(weather);
-        }
-
-        private double ConvertKelvinToCelsius(double Kelvin)
-        {
-            return (Kelvin - 273.15);
+            return View();
         }
 
         public IActionResult Privacy()
